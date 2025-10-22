5d334b6c4621bf7a2839330d89dc108886cd5b23
HenryWHM <whm2903@gmail.com>
2025-10-23 00:19:39 +1100

    Finally got the .aab file after days!

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,7 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
@@ -11,16 +11,16 @@ plugins {
 
 android {
     namespace = "com.whatsforkoi.app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13113456-rc1"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -28,34 +28,35 @@ android {
         applicationId = "com.whatsforkoi.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     // Load keystore properties
-    val keystoreProperties = Properties()
     val keystorePropertiesFile = rootProject.file("key.properties")
     if (keystorePropertiesFile.exists()) {
-        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-    }
-
-    if (keystorePropertiesFile.exists()) {
+        val keystoreProperties = Properties().apply {
+            load(FileInputStream(keystorePropertiesFile))
+        }
         signingConfigs {
             create("release") {
-                keyAlias = keystoreProperties["keyAlias"]?.toString() ?: ""
-                keyPassword = keystoreProperties["keyPassword"]?.toString() ?: ""
-                storeFile = keystoreProperties["storeFile"]?.toString()?.let { file(it) }
-                storePassword = keystoreProperties["storePassword"]?.toString() ?: ""
+                keyAlias = keystoreProperties.getProperty("keyAlias", "")
+                keyPassword = keystoreProperties.getProperty("keyPassword", "")
+                val storeFilePath = keystoreProperties.getProperty("storeFile", "")
+                storeFile = if (storeFilePath.isNotBlank()) file(storeFilePath) else null
+                storePassword = keystoreProperties.getProperty("storePassword", "")
             }
         }
-    }
-
-
-    buildTypes {
-        release {
-            // signingConfig = signingConfigs.getByName("release")
+        buildTypes {
+            release {
+                signingConfig = signingConfigs.getByName("release")
+            }
+        }
+    } else {
+        buildTypes {
+            release { /* unsigned */ }
         }
     }
 }
